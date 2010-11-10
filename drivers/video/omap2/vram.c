aac0b502e6457663fa2509a31592dc75f8220b12
Tomi Valkeinen <tomi.valkeinen@nokia.com>
2010-11-10 11:45:19 +0200

    OMAP: VRAM: Fix boot-time memory allocation
    Use memblock_free() and memblock_remove() to remove the allocated or
    reserved VRAM area from normal kernel memory.
    
    This is a slightly modified version of patches from Felipe Contreras and
    Namhyung Kim.
    
    Reported-by: Felipe Contreras <felipe.contreras@gmail.com>
    Reported-by: Namhyung Kim <namhyung@gmail.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/video/omap2/vram.c
+++ drivers/video/omap2/vram.c
@@ -609,9 +609,12 @@ void __init omap_vram_reserve_sdram_memblock(void)
 			return;
 		}
 	} else {
-		paddr = memblock_alloc_base(size, PAGE_SIZE, MEMBLOCK_REAL_LIMIT);
+		paddr = memblock_alloc(size, PAGE_SIZE);
 		if(paddr == NULL) return;
 	}
+
+	memblock_free(paddr, size);
+	memblock_remove(paddr, size);
 
 	omap_vram_add_region(paddr, size);
 
